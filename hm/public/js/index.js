a0049db2889e2b380c8ddb9c166d775f308ac0a5
汤瑞宇 <tang.rui.yu@foxmail.com>
2018-06-25 23:28:25 +0800

    add

--- hm/public/js/index.js
+++
@@ -1,8 +0,0 @@
-var table = document.querySelector(".info_more>.table");
-table.addEventListener("click",function (e) {
-if(e.target.classList.contains('add')){
-
-}else if(e.target.classList.contains("delete")){
-
-}
-})
